d84ccb678e5634d65c7d24694e60970cceff2b9a
Eddie Kao <eddie@digik.com.tw>
2015-04-10 19:44:00 +0800

    add settings logic config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,6 +234,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, "103089748609", "da907e56f4e9890cc51e893bade0"
+  config.omniauth :facebook, Settings.Facebook.APP_ID, Settings.Facebook.APP_SECRET
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
